97e7cad9ae321607c924e26bf5b32bc1bf16e079
Hammad Ali <hammadaali02@gmail.com>
2024-09-18 22:30:12 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      // title: 'Instagram 3d touch animation',
+      title: 'Instagram 3d touch animation',
       home: InstagramSearchTab(),
     ),
   );
